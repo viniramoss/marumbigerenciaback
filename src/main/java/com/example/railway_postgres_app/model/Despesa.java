9776dfa02ae3174f7caf55f33e65977f5f4464a1
Vini Ramos <viniramosdev@gmail.com>
2025-05-04 02:22:29 -0300

    VAI GT

--- src/main/java/com/example/railway_postgres_app/model/Despesa.java
+++ src/main/java/com/example/railway_postgres_app/model/Despesa.java
@@ -32,4 +32,77 @@ public class Despesa {
 
   @NotNull
   private Boolean pago = false;
+
+  // Getters e Setters para corrigir os erros de compilação
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDate getData() {
+    return data;
+  }
+
+  public void setData(LocalDate data) {
+    this.data = data;
+  }
+
+  public String getFornecedor() {
+    return fornecedor;
+  }
+
+  public void setFornecedor(String fornecedor) {
+    this.fornecedor = fornecedor;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  public String getUnidade() {
+    return unidade;
+  }
+
+  public void setUnidade(String unidade) {
+    this.unidade = unidade;
+  }
+
+  public String getBanco() {
+    return banco;
+  }
+
+  public void setBanco(String banco) {
+    this.banco = banco;
+  }
+
+  public String getMetodo() {
+    return metodo;
+  }
+
+  public void setMetodo(String metodo) {
+    this.metodo = metodo;
+  }
+
+  public BigDecimal getValor() {
+    return valor;
+  }
+
+  public void setValor(BigDecimal valor) {
+    this.valor = valor;
+  }
+
+  public Boolean getPago() {
+    return pago;
+  }
+
+  public void setPago(Boolean pago) {
+    this.pago = pago;
+  }
 }
