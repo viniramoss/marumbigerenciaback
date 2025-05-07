79fbcc20a9420d652024aba88cf27700d4d17fd6
Vini Ramos <viniramosdev@gmail.com>
2025-05-07 15:05:13 -0300

    teste apenas

--- src/main/java/com/example/railway_postgres_app/repository/DespesaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.railway_postgres_app.repository;
-
-import com.example.railway_postgres_app.model.Despesa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DespesaRepository extends JpaRepository<Despesa, Long> {}
